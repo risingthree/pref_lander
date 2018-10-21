c13e90893336cb294daa9a3b2144a6f0eeb8d537
Michael Brennan <brennan3@gmail.com>
2018-10-20 22:56:23 -0400

    Wire up the contact us form

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+$( "#contact-us-form" ).submit(function( event ) {
+  event.preventDefault();
+
+  var fname = $('#fname').val(),
+  lname = $('#lname').val(),
+  email = $('#email').val(),
+  ctry = $('#ctry').val(),
+  $form = $(this),
+  url = $form.attr("action"),
+  data = {
+    email: email,
+    ctry: ctry,
+    fname: fname,
+    lname: lname
+  };
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    dataType: 'json'
+  });
+
+  $('#myModal').modal('hide');
+  alert('Thank you for your inquiry.  We will contact you shortly!');
+});
